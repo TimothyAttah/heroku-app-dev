f4ba4fc4c9da7f570c0d2d726b5edfcf717ac5fe
Timothy Attah <timothyattah733@gmail.com>
2021-03-11 02:46:45 +0100

    Server Setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require( 'express' );
+const cors = require( 'cors' );
+
+const app = express();
+
+app.use( cors() );
+
+app.get( '/', ( req, res ) => {
+  res.send( 'Hello Beautiful world' )
+} );
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=> console.log(`Server started on port: ${PORT}`))
