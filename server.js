e96184a343de5567c8834e3be4ce31991060e740
Timothy Attah <timothyattah733@gmail.com>
2021-03-11 22:14:29 +0100

    added homepage route

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get( '/', ( req, res ) => {
   res.send( 'Hello Beautiful world, how are you!!!' )
 } );
 
+app.get( '/home', ( req, res ) => {
+  res.send('This is home page')
+})
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, ()=> console.log(`Server started on port: ${PORT}`))
