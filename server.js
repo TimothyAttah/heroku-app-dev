522b0da43201dadd02b5e762299694a21ea21fae
Timothy Attah <timothyattah733@gmail.com>
2021-03-15 22:05:12 +0100

    change urlencoded in app.use

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require( 'cors' );
 
 const app = express();
 
-app.use(express.urlencoded(), express.static({}))
+app.use( express.urlencoded( { extended: true } ) );
 app.use( cors() );
 
 app.get( '/', ( req, res ) => {
