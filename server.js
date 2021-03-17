bf95e76441a43d93bda599b1431d9762e7639ac0
Timothy Attah <timothyattah733@gmail.com>
2021-03-17 19:14:28 +0100

    Eslint setup

--- server.js
+++ server.js
@@ -3,25 +3,24 @@ const cors = require( 'cors' );
 
 const app = express();
 
-app.use(express.json())
+app.use( express.json() );
 app.use( express.urlencoded( { extended: true } ) );
 app.use( cors() );
 
 app.get( '/', ( req, res ) => {
-  res.send( 'Hello Beautiful world, how are you!!!' )
+  res.send( 'Hello Beautiful world, how are you!!!' );
 } );
 
 app.get( '/home', ( req, res ) => {
-  res.send('This is home page')
-} )
-
+  res.send( 'This is home page' );
+} );
 
 app.post( '/home', ( req, res ) => {
-  const show = req.body
-  console.log( show )
-  res.status(201).json({message: 'OK'})
-})
+  const show = req.body;
+  console.log( show );
+  res.status( 201 ).json( { message: 'OK' } );
+} );
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, ()=> console.log(`Server started on port: ${PORT}`))
+app.listen( PORT, () => console.log( `Server started on port: ${ PORT }` ) );
